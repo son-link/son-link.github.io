1420af78a051a58020e4f0e2399fe1c8ada68ffa
Alfonso Saavedra Son <sonlink.dourden@gmail.com>
2016-07-13 18:42:43 +0200

    Nueva entrada del tuto de Jekyll y algunos cambios en el diseño

--- js/archive.js
+++ js/archive.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$('.showHideMonth').click(function(e){
 		e.preventDefault();
 		$(this).parent().next().toggleClass('visible');
-		$('i', this).toggleClass('icon-down-dir');
+		$('i', this).toggleClass('icon-down-open');
 	});
 	$('#menu').click(function(e){
 		e.preventDefault();
